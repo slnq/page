ecea4acc8d087849117200715e285ad03b0b2708
GitJournal <app@gitjournal.io>
2025-09-26 00:31:09 +0900

    Updated Note post/2025-09-26-00-11-41.md

---
+++ tagger_util.py
@@ -0,0 +1,48 @@
+import fugashi
+
+# タグ抽出関数
+def extract_tags(text: str, stopwords_path: str = "stopwords.txt"):
+    with open(stopwords_path, encoding="utf-8") as f:
+        stopwords = set(f.read().strip().split(","))
+
+    tagger = fugashi.Tagger()
+    words = list(tagger(text))
+
+    nouns = []
+    buffer = ""
+    keijoshi_buffer = ""
+
+    for word in words:
+        f = word.feature
+
+        is_sahen = f.pos1 == "名詞" and f.pos3 == "サ変可能"
+        is_valid_noun = f.pos1 == "名詞" and f.pos3 not in ["副詞可能", "形状詞"]
+        is_valid_suffix = (
+            f.pos1 == "接尾辞" and f.pos2 == "名詞的" and f.pos3 != "副詞可能"
+        )
+        is_keijoshi = f.pos1 == "形状詞"
+
+        if is_valid_noun or is_sahen:
+            if keijoshi_buffer:
+                buffer += keijoshi_buffer
+                keijoshi_buffer = ""
+            buffer += word.surface
+        elif is_valid_suffix:
+            if keijoshi_buffer:
+                buffer += keijoshi_buffer
+                keijoshi_buffer = ""
+            buffer += word.surface
+        elif is_keijoshi:
+            keijoshi_buffer += word.surface
+        else:
+            if buffer:
+                if buffer not in stopwords:
+                    nouns.append(buffer)
+                buffer = ""
+            keijoshi_buffer = ""
+
+    if buffer:
+        if buffer not in stopwords:
+            nouns.append(buffer)
+
+    return nouns
